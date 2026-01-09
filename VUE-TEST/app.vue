f44a7ee51436d3b5f6cfcfd80d13944bad1f9060
Blackgoat0103 <blackgoat0103@gmail.com>
2026-01-09 17:41:13 +0800

    tried to fix the navbar
    tried to fix the navbar

---
+++ VUE-TEST/app.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+
+//data
+const name = ref('Rachel');
+const href = ref('https://www.scrimba.com');
+const disabled = ref(true);
+const attributes = {
+  id: 'big_emoji',
+  class: 'waving',
+};
+const coding_years = ref(0);
+const getFormattedDate = (date) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return date.toLocaleDateString('en-UK', options);
+};
+const username = ref('rachelktyjohnson');
+const event = ref('dblclick');
+const blockLeave = () => {
+  alert("You can't leave!");
+};
+</script>
+
+<template>
+  <span>👋🏻</span>
+  <h1>Hi, my name is {{ name }}</h1>
+  <a v-bind:href="href" target="_blank">
+    <!--
+    CHALLENGE: Bind the button's disabled attribute to the disabled variable
+    SYNTAX: v-bind:[html attribute]="variable from script section"
+    -->
+    <button>Click to visit my favourite website! If you can...</button>
+  </a>
+  <h2>I have been coding for 100 years</h2>
+  <h3>Today is 29 February 1993</h3>
+</template>
